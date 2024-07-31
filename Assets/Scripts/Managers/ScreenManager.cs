36b47529a2e8f89340b41ae092aa197bb7968a0e
rmsepskek02 <rmsepskek02@gmail.com>
2024-07-31 17:46:51 +0900

    화면 해상도 관련
     - 1920 1080 에 맞추어 작업
     - 창 모드 관련, 가로 해상도 조절 관련 추가 작업 필요

---
+++ Assets/Scripts/Managers/ScreenManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    int deviceWidth; 
+    int deviceHeight;
+    int setWidth;
+    int setHeight;
+    private void Start()
+    {
+        setWidth = 1920;
+        setHeight = 1080;
+    }
+    private void Update()
+    {
+        Debug.Log("deviceWidth = " + deviceWidth);
+        Debug.Log("deviceHeight = " + deviceHeight);
+        SetResolution(setWidth, setHeight);
+    }
+    /* 해상도 설정하는 함수 */
+    public void SetResolution(int setWidth, int setHeight)
+    {
+        deviceWidth = Screen.width; // 기기 너비 저장
+        deviceHeight = Screen.height; // 기기 높이 저장
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), false); // SetResolution 함수 제대로 사용하기
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
+    }
+}
